cb2065eb030b0f2856c2ec1a04f8e5cc08d8cd3d
1811501642-Kalam <73571222+kingkhanthekk@users.noreply.github.com>
2023-07-09 15:51:27 +0300

    Added necessary files and data, styles, and code to render pizza menu page.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,107 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+const App = () => {
+  return (
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+};
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Khan Pizza Co.</h1>
+    </header>
+  );
+};
+
+const Menu = () => {
+  return (
+    <div className="menu">
+      <h2>Our Menu</h2>
+      <ul className="pizzas">
+        {pizzaData.map((pizza) => (
+          <Pizza pizzaInfo={pizza} key={pizza.name} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const Pizza = (props) => {
+  return (
+    <li className="pizza">
+      <img src={props.pizzaInfo.photoName} alt={props.pizzaInfo.name} />
+      <div>
+        <h3>{props.pizzaInfo.name}</h3>
+        <p>{props.pizzaInfo.ingredients}</p>
+        <span>Price: {props.pizzaInfo.price}</span>
+      </div>
+    </li>
+  );
+};
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>We're currently open.</p>
+    </footer>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
